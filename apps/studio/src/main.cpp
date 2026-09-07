e119160fc4126c11d1f87869b985d4ba5c54dc0d
helton-godoy <chileno.brasil@gmail.com>
2026-09-06 22:30:43 -0300

    feat(studio): suporta --version e --help para smoke de instalação

--- apps/studio/src/main.cpp
+++ apps/studio/src/main.cpp
@@ -1,8 +1,18 @@
 #include "gui/MainWindow.h"
 #include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  QApplication::setApplicationName("showbox-studio");
+  QApplication::setApplicationVersion("1.0.0");
+
+  QCommandLineParser commandLine;
+  commandLine.setApplicationDescription("Visual editor for ShowBox interfaces.");
+  commandLine.addHelpOption();
+  commandLine.addVersionOption();
+  commandLine.process(a);
 
   MainWindow w;
   w.show();
